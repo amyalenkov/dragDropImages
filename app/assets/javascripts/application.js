e3c8d820fb5df630083b49444d2b3d27e794bd2f
amyalenkov <stmy999@gmail.com>
2014-10-19 17:05:08 +0300

    clear part of canvas

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,6 +18,7 @@
 //= require jquery
 //= require_tree .
 $(document).ready(function () {
+    $(".clear_div").hide()
     $(".template_div").hide()
     $("#div_template1").show()
 
@@ -60,51 +61,34 @@ $(document).ready(function () {
             $(".template_div").hide()
             $(id).show()
         });
-
+    $('.clear_div').click(function(){
+        var canvas = document.getElementById('canvas');
+        var context = canvas.getContext('2d');
+        var x = $(this).parent().attr('x')
+        var y = $(this).parent().attr('y')
+        var h = $(this).parent().height()
+        var w = $(this).parent().width()
+        clear(context, canvas, x, y, w, h)
+    })
+    $('.droppableImg').mouseover(function(){
+        console.log($(this).attr('x') +"-"+ $(this).attr('y') + ' enter')
+        $(this).children().show();
+    })
+    $('.droppableImg').mouseleave(function(){
+        console.log($(this).attr('x') +"-"+ $(this).attr('y') + ' leave')
+        $(this).children().hide();
+    })
 });
-var zoom = 50;
-function zoom_in(){
-    var canvas = document.getElementById('canvas');
-    var context = canvas.getContext('2d');
-    context.scale(2,2)
-    i = i;
-    hw = hw
-    console.log('zoom in ' + i)
-    console.log('zoom in ' + hw)
-    console.log('zoom in ' + canvas.height)
-    w = canvas.width + hw;
-    h = canvas.height + hw;
-    drawImage(canvas,canvas.toDataURL(),i, i,w ,h )
-}
-
-var zoomDelta = 0.5;
-var currentScale = 1;
-var i = 1;
-var hw = 0;
-function zoom_out(){
-    var canvas = document.getElementById('canvas');
-    var context = canvas.getContext('2d');
-
-    i = i;
-    hw = hw
-    context.scale(1,1)
-
-    console.log('zoom out ' + i)
-    console.log('zoom out ' + canvas.width)
-    w = canvas.width + hw;
-    h = canvas.height + hw;
-    drawImage(canvas,canvas.toDataURL(),i, i,w ,h )
-}
 
 function delete_img(){
     console.log('delete_img')
     var canvas = document.getElementById('canvas');
     var context = canvas.getContext('2d');
-    clear(context, canvas)
+    clear(context, canvas, 0, 0, canvas.width, canvas.height)
 }
 
-function clear(context, canvas) {
-    context.clearRect(0, 0, canvas.width, canvas.height);
+function clear(context, canvas, x, y, w, h) {
+    context.clearRect(x, y, w, h);
 }
 
 
@@ -118,44 +102,4 @@ function drawImage(canvas, src, x, y, w, h) {
     imageObj.src = src
 
     context.restore();
-}
-
-var startX, startY, isDown = false;
-
-function getMousePos(canvas, evt) {
-    var rect = canvas.getBoundingClientRect();
-    return {
-        x: evt.clientX - rect.left,
-        y: evt.clientY - rect.top
-    };
-}
-canvas = document.getElementById('canvas');
-canvas.onmousedown = function (e) {
-    var pos = getMousePos(canvas, e);
-    startX = pos.x;  //store current position
-    startY = pos.y;
-    console.log('ddd')
-    isDown = true;   //mark that we are in move operation
-}
-
-canvas.onmousemove = function (e) {
-    if (isDown === true) {
-        var pos = getMousePos(canvas, e);
-        var x = pos.x;
-        var y = pos.y;
-        var context = canvas.getContext('2d');
-
-        //translate difference from now and start
-        context.translate(x - startX, y - startY);
-        drawImage(canvas,canvas.toDataURL(),0,0,375,175);
-
-        //update start positions for next loop
-        startX = x;
-        startY = y;
-    }
-}
-
-//reset move operation status
-canvas.onmouseup = function (e) {
-    isDown = false;
 }
