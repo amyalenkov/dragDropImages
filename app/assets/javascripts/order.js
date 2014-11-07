12b856f89f61ef08cd99f2f4a3fcf5e071becb0a
amyalenkov <stmy999@gmail.com>
2014-11-07 14:47:53 +0300

    remake and fix https://github.com/amyalenkov/dragDropImages/issues/2

--- app/assets/javascripts/order.js
+++ app/assets/javascripts/order.js
@@ -5,4 +5,22 @@ $(document).ready(function () {
         var finalCost =  (+number) * (+oneCost);
         $('#finalCost').val(finalCost);
     })
+    $('#plus_count').click(function(){
+        var oldNumber =  $("#count").val();
+        var newNumber = 1 + (+oldNumber);
+        var oneCost = $('#cost').val();
+        var finalCost =  (+newNumber) * (+oneCost);
+        $('#finalCost').val(finalCost);
+        $('#count').val(newNumber);
+    });
+    $('#minus_count').click(function(){
+        var oldNumber =  $("#count").val();
+        if (oldNumber != 1){
+            var newNumber = (+oldNumber) - 1;
+            var oneCost = $('#cost').val();
+            var finalCost =  (+newNumber) * (+oneCost);
+            $('#finalCost').val(finalCost);
+            $('#count').val(newNumber);
+        }
+    });
 });
