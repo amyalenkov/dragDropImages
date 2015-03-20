f923cab04bdd16c6b083a14ba1e4b88443929da8
amyalenkov <stmy999@gmail.com>
2015-03-20 20:26:22 +0300

    fix problem with auth user

--- app/controllers/paintings_controller.rb
+++ app/controllers/paintings_controller.rb
@@ -46,14 +46,21 @@ class PaintingsController < ApplicationController
   def get_user
     token = cookies['image_user_token']
     if token == nil
-      new_token = User.new_remember_token
-      encrypt = User.encrypt(new_token)
-      @user = User.create
-      @user.token = encrypt
-      @user.save
-      cookies['image_user_token'] = new_token
+      create_new_token_and_user
     else
       @user = User.find_by_token(User.encrypt(token))
+      if @user.nil?
+        create_new_token_and_user
+      end
     end
+  end
+
+  def create_new_token_and_user
+    new_token = User.new_remember_token
+    encrypt = User.encrypt(new_token)
+    @user = User.create
+    @user.token = encrypt
+    @user.save!
+    cookies['image_user_token'] = new_token
   end
 end
